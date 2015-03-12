deedd0e1445d0363aae50d04cb1f7fd7723f7a4d
vanrado <radovan.racak@gmail.com>
2015-03-12 21:58:56 +0100

    Doplnenie statickej metody do enumu EIssuePriority

--- src/main/java/snech/core/types/enums/EIssuePriority.java
+++ src/main/java/snech/core/types/enums/EIssuePriority.java
@@ -1,9 +1,27 @@
 package snech.core.types.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Radovan Račák
  */
 public enum EIssuePriority {
     A, B, C, D, E;
+
+    private EIssuePriority() {
+    }
+    
+    public static List<String> getPrioritiesString(){
+        List<String> list = new ArrayList<String>();
+        EIssuePriority[] priorities = EIssuePriority.values();
+        
+        for(EIssuePriority priority : priorities){
+            list.add(priority.name());
+        }
+        
+        return list;
+    }
+    
 }

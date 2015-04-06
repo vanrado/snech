12fea348f93accdeae18124a4274e75b3f71088c
vanrado <radovan.racak@gmail.com>
2015-04-06 21:52:44 +0200

    Pridana podpora pre upload a mazanie priloh pre obrazovku TicketEditPage

--- src/main/java/snech/web/forms/TicketEditForm.java
+++ src/main/java/snech/web/forms/TicketEditForm.java
@@ -16,12 +16,16 @@
 package snech.web.forms;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.wicket.Application;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
@@ -31,6 +35,8 @@ import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.upload.FileUpload;
+import org.apache.wicket.markup.html.form.upload.MultiFileUploadField;
 import org.apache.wicket.markup.html.link.DownloadLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -40,7 +46,11 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.file.Files;
+import org.apache.wicket.util.file.Folder;
+import snech.WicketApplication;
 import snech.core.services.IDatabaseService;
+import snech.core.services.IHashUtils;
 import snech.core.types.Attachment;
 import snech.core.types.Issue;
 import snech.core.types.enums.EIssuePriority;
@@ -56,13 +66,19 @@ public class TicketEditForm extends Form<Object> {
     @SuppressWarnings("FieldMayBeFinal")
     @SpringBean
     private IDatabaseService databaseService;
+
+    @SpringBean
+    private IHashUtils hashUtils;
+
     private String subject;
     private String selectedPriority;
     private String message;
     private Issue issue;
     private final String UNKNOWN = "-";
-    private List<Long> attachmentsToDelete;
+    private List<Attachment> attachmentsToDelete;
     private List<Attachment> attachments;
+    private Collection<FileUpload> uploads;
+    private MultiFileUploadField uploadField;
 
     public TicketEditForm(String id) {
         super(id);
@@ -70,9 +86,10 @@ public class TicketEditForm extends Form<Object> {
 
     public TicketEditForm(String id, final PageParameters pageParameters) {
         super(id);
+        uploads = new ArrayList<>();
         attachmentsToDelete = new ArrayList<>();
         issue = databaseService.getIssue(Long.parseLong(pageParameters.get("id").toString()));
-        attachments = databaseService.getAttachments(issue.getId(), -1);
+        attachments = attachments == null ? new ArrayList<Attachment>() : databaseService.getAttachments(issue.getId(), -1);
         subject = issue.getSubject();
         selectedPriority = issue.getPriority().name();
         message = issue.getMessage();
@@ -94,45 +111,13 @@ public class TicketEditForm extends Form<Object> {
         messageTextArea.setRequired(true);
         add(messageTextArea);
 
-        add(new Button("save.button") {
+        setMultiPart(true);
+        uploadField = new MultiFileUploadField("fileInput", new PropertyModel<Collection<FileUpload>>(this, "uploads"), 5);
 
-            @Override
-            public void onError() {
-                super.onError();
-                error("Error on submit");
-            }
+        add(uploadField);
 
-            @Override
-            public void onSubmit() {
-                super.onSubmit();
-                issue.setSubject(subject);
-                issue.setPriority(EIssuePriority.valueOf(selectedPriority));
-                issue.setMessage(message);
-                databaseService.updateIssue(issue);
-                for (Long attachmentId : attachmentsToDelete) {
-                    long id = Long.valueOf(attachmentId);
-                    System.out.println(id);
-                    if (databaseService.removeAttachment(id)) {
-                        info("Uspesne vymazana priloha #" + attachmentId);
-                    } else {
-                        error("Chyba pri mazani prilohy #" + attachmentId);
-                    }
+        add(new Button("save.button"));
 
-                }
-                info("Uspesne aktualizovane, na vymazanie " + attachmentsToDelete.size());
-                attachmentsToDelete.clear();
-//                List<Attachment> newAttachments = databaseService.getAttachments(issue.getId(), -1);
-//                replaceContainer(newAttachments);
-
-            }
-
-            @Override
-            public void onAfterSubmit() {
-                setResponsePage(TicketEditPage.class, pageParameters);
-            }
-
-        });
-        
         add(new Link("cancel.button") {
 
             @Override
@@ -141,69 +126,138 @@ public class TicketEditForm extends Form<Object> {
             }
 
         });
-        
-        if (attachments != null) {
-            add(new ListView<Attachment>("attachments", attachments) {
 
-                @Override
-                protected void populateItem(ListItem<Attachment> listItem) {
-                    try {
-                        final Attachment attachment = listItem.getModelObject();
-                        File file = new File(attachment.getFileUrl() != null ? URLDecoder.decode(attachment.getFileUrl(), "UTF-8") : UNKNOWN);
-                        DownloadLink downloadLink = new DownloadLink("attLink", file, URLDecoder.decode(attachment.getFileName(), "UTF-8"));
-                        downloadLink.add(new Label("fileName", attachment.getFileName()));
-                        listItem.add(downloadLink);
+        ListView<Attachment> attachmentsView;
 
-                        AjaxLink removeLink = new AjaxLink("remove.link") {
+        add(new ListView<Attachment>("attachments", attachments) {
 
-                            @Override
-                            public void onClick(AjaxRequestTarget target) {
+            @Override
+            protected void populateItem(ListItem<Attachment> listItem) {
+                try {
+                    final Attachment attachment = listItem.getModelObject();
+                    File file = new File(attachment.getFileUrl() != null ? URLDecoder.decode(attachment.getFileUrl(), "UTF-8") : UNKNOWN);
+                    DownloadLink downloadLink = new DownloadLink("attLink", file, URLDecoder.decode(attachment.getFileName(), "UTF-8"));
+                    downloadLink.add(new Label("fileName", attachment.getFileName()));
+                    listItem.add(downloadLink);
 
-                                if (attachmentsToDelete.contains(attachment.getId())) {
-                                    attachmentsToDelete.remove(attachment.getId());
-                                } else {
-                                    attachmentsToDelete.add(attachment.getId());
-                                }
-                                info("dlzka pola na vymazanie priloh: " + attachmentsToDelete.toArray().length);
-                                target.add(feedback);
+                    AjaxLink removeLink = new AjaxLink("remove.link") {
+
+                        @Override
+                        public void onClick(AjaxRequestTarget target) {
+
+                            if (attachmentsToDelete.contains(attachment)) {
+                                attachmentsToDelete.remove(attachment);
+                            } else {
+                                attachmentsToDelete.add(attachment);
                             }
-                        };
-                        listItem.add(removeLink);
-                    } catch (UnsupportedEncodingException ex) {
-                        Logger.getLogger(TicketDetailPage.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-            });
-        } else {
-            //Component pridany, ale nieje nutne zobrazovat nejake prilohy
-            MarkupContainer attachmentContainer = new MarkupContainer("attachments") {
-                @Override
-                protected void onInitialize() {
-                    super.onInitialize();
-                    add(new Link("attLink") {
-
-                        @Override
-                        protected void onInitialize() {
-                            super.onInitialize();
-                            add(new Label("fileName", UNKNOWN));
+                            target.add(feedback);
                         }
-
-                        @Override
-                        public void onClick() {
-
-                        }
-                    }.setVisible(false));
+                    };
+                    listItem.add(removeLink);
+                } catch (UnsupportedEncodingException ex) {
+                    Logger.getLogger(TicketDetailPage.class.getName()).log(Level.SEVERE, null, ex);
                 }
+            }
+        });
+    }
 
-            };
-            attachmentContainer.add(new Link("remove.link") {
+    @Override
+    protected void onSubmit() {
+        super.onSubmit();
+        issue.setSubject(subject);
+        issue.setPriority(EIssuePriority.valueOf(selectedPriority));
+        issue.setMessage(message);
+        databaseService.updateIssue(issue);
 
-                @Override
-                public void onClick() {
-                }
-            }.setVisible(false));
-            
-            add(attachmentContainer);
+        removeAttachments();
+        uploadAttachments();
+
+        if (attachments != null) {
+            attachments.clear();
+            attachments.addAll(databaseService.getAttachments(issue.getId(), -1));
         }
+        attachmentsToDelete.clear();
+    }
+
+    private Folder getUploadFolder() {
+        return ((WicketApplication) Application.get()).getUploadFolder();
+    }
+
+    /**
+     * Check whether the file allready exists, and if so, try to delete it.
+     *
+     * @param file
+     */
+    private void checkFileExists(File file) {
+        if (file.exists()) {
+
+            //Try to delete file
+            if (!Files.remove(file)) {
+                throw new IllegalStateException("Unable to overwrite " + file.getAbsolutePath());
+            }
+        }
+    }
+
+    private void uploadAttachments() {
+        Folder newFolder;
+        try {
+            //Vytvorit Folder pre tento upload - current value of seq
+            newFolder = new Folder(getUploadFolder(), databaseService.getUploadsCount() + "-" + hashUtils.randomStringGenerator(5));
+            newFolder.mkdirs();
+
+            //Doneho zapisem subory
+            for (FileUpload fileUpload : uploads) {
+                String fileName = fileUpload.getClientFileName();
+                File newFile = new File(newFolder, fileName);
+
+                // Skontroluj ci subor uz neexistuje, ak ano vymaz ho a nahrad
+                checkFileExists(newFile);
+
+                try {
+                    newFile.createNewFile();
+                    fileUpload.writeTo(newFile);
+                    info("Subor " + fileUpload.getClientFileName() + " ulozeny do: " + newFile.getAbsolutePath());
+                } catch (IOException ex) {
+                    Logger.getLogger(ReportForm.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (Exception ex) {
+                    Logger.getLogger(ReportForm.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                Attachment attachment = new Attachment();
+                String fileNameEncoded = URLEncoder.encode(fileName, "UTF-8");
+                attachment.setFileName(fileNameEncoded);
+                //attachment.setFileSize(newFile.get);
+                String url = newFile.getAbsolutePath();
+                String encodedUrl = URLEncoder.encode(url, "UTF-8");
+                attachment.setFileUrl(encodedUrl);
+                attachment.setIssueId(issue.getId());
+                attachment.setMessageId(null);
+
+                try {
+                    boolean success = databaseService.insertAttachment(attachment);
+                } catch (Exception ex) {
+                    Logger.getLogger(ReportForm.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        } catch (Exception ex) {
+            error("Chyba pri uploade suboru!");
+        }
+    }
+
+    private void removeAttachments() {
+        for (Attachment attachment : attachmentsToDelete) {
+            if (databaseService.removeAttachment(attachment.getId())) {
+                info("Uspesne vymazana priloha #" + attachment.getId());
+
+            } else {
+                error("Chyba pri mazani prilohy #" + attachment.getId());
+            }
+
+        }
+    }
+
+    public Collection<FileUpload> getUploads() {
+        return uploads;
     }
 }

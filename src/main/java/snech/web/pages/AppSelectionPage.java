dc8e4cb07fa489ed79bcff3cd6a94f877f432382
vanrado <radovan.racak@gmail.com>
2015-04-07 21:09:10 +0200

    Pridanie obrazovky Rozcestnik - pri prihlasovani spravcu/technika je na vyber do ktorej aplikacie chce vstupit

---
+++ src/main/java/snech/web/pages/AppSelectionPage.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 vanrado.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package snech.web.pages;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
+import org.apache.wicket.markup.html.link.Link;
+import snech.core.CustomAuthenticatedWebSession;
+import snech.web.base.BasePage;
+import snech.web.pages.client.TicketDetailPage;
+import snech.web.pages.client.TicketsListPage;
+import snech.web.panels.admin.AdminHeaderPanel;
+
+/**
+ *
+ * @author vanrado
+ */
+public class AppSelectionPage extends BasePage {
+    
+    public AppSelectionPage() {
+        add(new AdminHeaderPanel("header.panel"));
+        add(new Link("clientApp.link") {
+            
+            @Override
+            public void onClick() {
+                setResponsePage(TicketsListPage.class);
+            }
+        });
+        
+        add(new Link("adminApp.link") {
+            
+            @Override
+            public void onClick() {
+                setResponsePage(AppSelectionPage.class);
+            }
+        });
+    }
+    
+    @Override
+    protected void onConfigure() {
+        AuthenticatedWebApplication app = (AuthenticatedWebApplication) Application.get();
+        if (!CustomAuthenticatedWebSession.get().isSignedIn()) {
+            app.restartResponseAtSignInPage();
+        }
+        
+        if (CustomAuthenticatedWebSession.get().getUser().getLogin().equals("robert_u")) {
+            setResponsePage(TicketsListPage.class);
+        }
+    }
+}
---
+++ src/main/java/snech/web/panels/admin/AdminHeaderPanel.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 vanrado.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package snech.web.panels.admin;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import snech.core.CustomAuthenticatedWebSession;
+
+/**
+ *
+ * @author vanrado
+ */
+public class AdminHeaderPanel extends Panel {
+
+    public AdminHeaderPanel(String id) {
+        super(id);
+        final CustomAuthenticatedWebSession mySession = CustomAuthenticatedWebSession.get();
+        add(new Link("signOut.link") {
+
+            @Override
+            public void onClick() {
+                mySession.invalidate();
+                setResponsePage(getApplication().getHomePage());
+            }
+
+        });
+
+        add(new Label("logedUser", mySession.getUser() != null ? mySession.getUser().getFirstName() + " " + mySession.getUser().getLastName() : ""));
+    }
+
+}

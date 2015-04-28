6e97ffea3879940497dd5eb7b0b992c574341080
vanrado <radovan.racak@gmail.com>
2015-04-28 13:07:40 +0200

    Doplnenie filtru na filtrovanie poziadaviek, zatial len html

--- src/main/webapp/web/js/main.js
+++ src/main/webapp/web/js/main.js
@@ -18,4 +18,16 @@ function initMyDatePicker() {
 $(document).ready(function () {
     initMyDatePicker();
     markForDelete();
+    $("#filter").hide();
+    $("#filterShowLink").click(function () {
+        if ($("#filter").css('display') === 'none') {
+            $("#filterShowLink span").removeClass("glyphicon-plus");
+            $("#filterShowLink span").addClass("glyphicon-minus");
+        } else {
+            $("#filterShowLink span").removeClass("glyphicon-minus");
+            $("#filterShowLink span").addClass("glyphicon-plus");
+        }
+
+        $("#filter").slideToggle();
+    });
 });
